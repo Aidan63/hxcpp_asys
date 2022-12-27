e96b095f157ba03e738d960e2b5a818b62d73f3a
Aidan Lee <aidan.lee63@gmail.com>
2022-12-27 15:05:44 +0000

    File system level file read and write functions

--- src/asys/native/filesystem/FileSystem.hx
+++ src/asys/native/filesystem/FileSystem.hx
@@ -1,5 +1,7 @@
 package asys.native.filesystem;
 
+import haxe.NoData;
+import haxe.io.Bytes;
 import sys.thread.Thread;
 
 class FileSystem {
@@ -18,7 +20,7 @@ class FileSystem {
             @:privateAccess Thread.current().events.context,
             path,
             cast flag,
-            file -> callback.success(cast @:privateAccess new File(path, file)),
+            file -> callback.success(cast @:privateAccess new File(file)),
             msg -> callback.fail(new FsException(msg, path)));
     }
 
@@ -37,6 +39,105 @@ class FileSystem {
 			file -> callback.success(cast @:privateAccess new File(file)),
 			msg -> callback.fail(new FsException(msg, '')));
 	}
+
+	/**
+		Read the contents of a file specified by `path`.
+	**/
+	static public function readBytes(path:String, callback:Callback<Bytes>):Void {
+		openFile(path, Read, (error, file) -> {
+			switch error {
+				case null:
+					file.info((error, stat) -> {
+						switch error {
+							case null:
+								final buffer = Bytes.alloc(stat.size);
+
+								file.read(0, buffer, 0, buffer.length, (error, read) -> {
+									file.close((_, _) -> {
+										// TODO : What should we do if closing fails?
+										// create a composite exception?
+										
+										switch error {
+											case null:
+												final output = if (read < buffer.length) {
+													buffer.sub(0, read);
+												} else {
+													buffer;
+												}
+
+												callback.success(output);
+											case exn:
+												callback.fail(exn);
+										}
+									});
+								});
+							case exn:
+								callback.fail(exn);
+						}
+					});
+				case exn:
+					callback.fail(exn);
+			}
+		});
+	}
+
+	/**
+		Read the contents of a file specified by `path` as a `String`.
+		TODO:
+		Should this return an error if the file does not contain a valid unicode string?
+	**/
+	static public function readString(path:String, callback:Callback<String>):Void {
+		readBytes(path, (error, bytes) -> {
+			switch error {
+				case null:
+					callback.success(bytes.toString());
+				case exn:
+					callback.fail(exn);
+			}
+		});
+	}
+
+	/**
+		Write `data` into a file specified by `path`
+		`flag` controls the behavior.
+		By default the file truncated if it exists and created if it does not exist.
+		@see asys.native.filesystem.FileOpenFlag for more details.
+	**/
+	static public function writeBytes(path:String, data:Bytes, flag:FileOpenFlag<Dynamic> = Write, callback:Callback<NoData>):Void {
+		openFile(path, flag, (error, file) -> {
+			switch error {
+				case null:
+					file.write(0, data, 0, data.length, (error, _) -> {
+						file.close((_, _) -> {
+							// TODO : What should we do if closing fails?
+							// create a composite exception?
+
+							switch error {
+								case null:
+									// Should we error if not all of the data was written?
+									callback.success(null);
+								case exn:
+									callback.fail(exn);
+							}
+						});
+					});
+				case exn:
+					callback.fail(exn);
+			}
+		});
+	}
+
+	/**
+		Write `text` into a file specified by `path`
+		`flag` controls the behavior.
+		By default the file is truncated if it exists and is created if it does not exist.
+		@see asys.native.filesystem.FileOpenFlag for more details.
+	**/
+	static public function writeString(path:String, text:String, flag:FileOpenFlag<Dynamic> = Write, callback:Callback<NoData>):Void {
+		writeBytes(path, Bytes.ofString(text), flag, callback);
+	}
+
+	/**
 		Open directory for listing.
 		`maxBatchSize` sets maximum amount of entries returned by a call to `directory.next`.
 		In general bigger `maxBatchSize` allows to iterate faster, but requires more

6ac39356e04f88546d447baed75fe3af02b71aad
Aidan Lee <aidan.lee63@gmail.com>
2023-01-15 18:32:53 +0000

    namespace re-shuffle

--- src/cpp/asys/Net.hx
+++ src/cpp/asys/Net.hx
@@ -5,18 +5,18 @@ import asys.native.system.AsysError;
 
 extern class Net
 {
-    @:native('::hx::asys::net::resolve')
+    @:native('::hx::asys::net::dns::resolve')
     static function resolve(ctx : Context, host : String, cbSuccess : Array<cpp.EnumBase>->Void, cbFailure : AsysError->Void) : Void;
 
-    @:native('::hx::asys::net::reverse')
+    @:native('::hx::asys::net::dns::reverse')
     static overload function reverse(ctx : Context, ip : Int, cbSuccess : String->Void, cbFailure : AsysError->Void) : Void;
 
-    @:native('::hx::asys::net::reverse')
+    @:native('::hx::asys::net::dns::reverse')
     static overload function reverse(ctx : Context, ip : BytesData, cbSuccess : String->Void, cbFailure : AsysError->Void) : Void;
 
-    @:native('::hx::asys::net::ipName')
+    @:native('::hx::asys::net::ip::name')
     static overload function ipName(ip : Int) : String;
 
-    @:native('::hx::asys::net::ipName')
+    @:native('::hx::asys::net::ip::name')
     static overload function ipName(ip : BytesData) : String;
 }

fc13c69dc00cdb5462db2fca4c143db06435017f
Aidan Lee <aidan.lee63@gmail.com>
2023-11-27 18:05:25 +0000

    Fix what I assume is an oversight with the file system callback typedef

--- src/asys/native/filesystem/Callback.hx
+++ src/asys/native/filesystem/Callback.hx
@@ -1,4 +1,6 @@
 package asys.native.filesystem;
 
+import asys.native.filesystem.FsException;
+
 //TODO: remove after https://github.com/HaxeFoundation/haxe-evolution/pull/50 is implemented
-typedef Callback<R> = haxe.Callback<haxe.Exception,R>;
+typedef Callback<R> = haxe.Callback<FsException,R>;

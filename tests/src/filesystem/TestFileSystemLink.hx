845296ccbd464ab74f4cfb5732a11a385b30ee72
Aidan Lee <aidan.lee63@gmail.com>
2023-12-18 19:52:21 +0000

    Add more hardlink tests

--- tests/src/filesystem/TestFileSystemLink.hx
+++ tests/src/filesystem/TestFileSystemLink.hx
@@ -7,6 +7,7 @@ import utest.Async;
 import utest.Assert;
 import asys.native.filesystem.FileSystem;
 
+@:depends(filesystem.TestFileSystemInfo)
 class TestFileSystemLink extends DirectoryTests {   
     function test_null_target(async:Async) {
         FileSystem.link(null, dummyFileName, SymLink, (error, _) -> {
@@ -78,6 +79,45 @@ class TestFileSystemLink extends DirectoryTests {
             }
 
             async.done();
+        });
+    }
+
+    function test_hardlink_stat_links(async:Async) {
+        FileSystem.link(dummyFileName, linkName, HardLink, (error, _) -> {
+            if (Assert.isNull(error)) {
+                FileSystem.info(linkName, (error, info) -> {
+                    if (Assert.isNull(error)) {
+                        Assert.equals(2, info.links);
+                    }
+
+                    async.done();
+                });
+            } else {
+                async.done();
+            }
+        });
+    }
+
+    function test_hardlink_stat_inode_number(async:Async) {
+        FileSystem.link(dummyFileName, linkName, HardLink, (error, _) -> {
+            if (Assert.isNull(error)) {
+                FileSystem.info(linkName, (error, linkInfo) -> {
+                    if (Assert.isNull(error)) {
+                        FileSystem.info(dummyFileName, (error, dummyInfo) -> {
+                            if (Assert.isNull(error)) {
+                                Assert.equals(linkInfo.inodeNumber, dummyInfo.inodeNumber);
+                            }
+
+                            async.done();
+                        });
+                    } else {
+                        async.done();
+                    }
+
+                });
+            } else {
+                async.done();
+            }
         });
     }
 

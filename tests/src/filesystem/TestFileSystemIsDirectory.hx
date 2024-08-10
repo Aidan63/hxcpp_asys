4c8237fde316056927d0cc901787ddc56be91d9f
Aidan Lee <aidan.lee63@gmail.com>
2024-08-10 12:00:25 +0100

    add a few more is file / directory checks for symlinks

--- tests/src/filesystem/TestFileSystemIsDirectory.hx
+++ tests/src/filesystem/TestFileSystemIsDirectory.hx
@@ -45,4 +45,37 @@ class TestFileSystemIsDirectory extends DirectoryTests {
             async.done();
         });
     }
+
+    function test_symlink_to_file(async:Async) {
+        FileSystem.link(dummyFileName, linkName, SymLink, (_, error) -> {
+            if (Assert.isNull(error)) {
+                FileSystem.isDirectory(linkName, (result, error) -> {
+                    if (Assert.isNull(error)) {
+                        Assert.isFalse(result);
+                    }
+
+                    async.done();
+                });
+            }
+            else {
+                async.done();
+            }
+        });
+    }
+
+    function test_symlink_to_directory(async:Async) {
+        FileSystem.link(directoryName, linkName, SymLink, (_, error) -> {
+            if (Assert.isNull(error)) {
+                FileSystem.isDirectory(linkName, (result, error) -> {
+                    if (Assert.isNull(error)) {
+                        Assert.isTrue(result);
+                    }
+
+                    async.done();
+                });
+            } else {
+                async.done();
+            }
+        });
+    }
 }
--- tests/src/filesystem/TestFileSystemIsFile.hx
+++ tests/src/filesystem/TestFileSystemIsFile.hx
@@ -45,4 +45,53 @@ class TestFileSystemIsFile extends DirectoryTests {
             async.done();
         });
     }
+
+    function test_symlink_to_file(async:Async) {
+        FileSystem.link(dummyFileName, linkName, SymLink, (_, error) -> {
+            if (Assert.isNull(error)) {
+                FileSystem.isFile(linkName, (result, error) -> {
+                    if (Assert.isNull(error)) {
+                        Assert.isTrue(result);
+                    }
+
+                    async.done();
+                });
+            }
+            else {
+                async.done();
+            }
+        });
+    }
+
+    function test_symlink_to_directory(async:Async) {
+        FileSystem.link(directoryName, linkName, SymLink, (_, error) -> {
+            if (Assert.isNull(error)) {
+                FileSystem.isFile(linkName, (result, error) -> {
+                    if (Assert.isNull(error)) {
+                        Assert.isFalse(result);
+                    }
+
+                    async.done();
+                });
+            } else {
+                async.done();
+            }
+        });
+    }
+
+    function test_hardlink_to_file(async:Async) {
+        FileSystem.link(dummyFileName, linkName, HardLink, (_, error) -> {
+            if (Assert.isNull(error)) {
+                FileSystem.isFile(linkName, (result, error) -> {
+                    if (Assert.isNull(error)) {
+                        Assert.isTrue(result);
+                    }
+
+                    async.done();
+                });
+            } else {
+                async.done();
+            }
+        });
+    }
 }

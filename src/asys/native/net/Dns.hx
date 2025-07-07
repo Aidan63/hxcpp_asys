9164719783ae292195dbb55bccf390b751bd83d0
Aidan Lee <aidan.lee@evcam.com>
2025-07-07 21:11:43 +0100

    coroutine dns functions

--- src/asys/native/net/Dns.hx
+++ src/asys/native/net/Dns.hx
@@ -7,6 +7,7 @@ import haxe.Callback;
 import haxe.io.Bytes;
 
 using Lambda;
+using hxcoro.util.Convenience;
 
 /**
 	Methods related to Domain Name System.
@@ -15,47 +16,47 @@ class Dns {
 	/**
 		Lookup the given `host` name.
 	**/
-	static public function resolve(host:String, callback:Callback<Array<Ip>>) {
-		if (callback == null) {
-			throw new ArgumentException("callback", "argument was null");
-		}
-
+	@:coroutine static public function resolve(host:String):Array<Ip> {
 		if (host == null) {
-			callback.fail(new ArgumentException("host", "argument was null"));
-
-			return;
+			throw new ArgumentException("host", "argument was null");
 		}
 
-		cpp.asys.Net.resolve(
-            @:privateAccess Thread.current().context(),
-            host,
-            ips -> callback.success(ips.map(convertIp)),
-            msg -> callback.fail(new IoException(msg.toIoErrorType())));
+		final ips = hxcoro.Coro.suspend(cont -> {
+			cpp.asys.Net.resolve(
+				cpp.asys.Context.get(),
+				host,
+				ips -> cont.succeedAsync(ips),
+				msg -> cont.failAsync(new IoException(msg.toIoErrorType())));
+		});
+
+		return ips.map(convertIp);
 	}
 
 	/**
 		Find host names associated with the given IP address.
 	**/
-	static public function reverse(ip:Ip, callback:Callback<Array<String>>) {
-		if (callback == null) {
-			throw new ArgumentException("callback", "argument was null");
-		}
-
+	@:coroutine static public function reverse(ip:Ip):Array<String> {
 		switch ip {
 			case null:
-				callback.fail(new ArgumentException("ip", "argument was null"));
+				throw new ArgumentException("ip", "argument was null");
 			case Ipv4(raw):
-				cpp.asys.Net.reverse(
-					@:privateAccess Thread.current().context(),
-					raw,
-					host -> callback.success([ host ]),
-					msg -> callback.fail(new IoException(msg.toIoErrorType())));
+				return
+					hxcoro.Coro.suspend(cont -> {
+						cpp.asys.Net.reverse(
+							cpp.asys.Context.get(),
+							raw,
+							host -> cont.succeedAsync([ host ]),
+							msg -> cont.failAsync(new IoException(msg.toIoErrorType())));
+					});
 			case Ipv6(raw):
-				cpp.asys.Net.reverse(
-					@:privateAccess Thread.current().context(),
-					raw.getData(),
-					host -> callback.success([ host ]),
-					msg -> callback.fail(new IoException(msg.toIoErrorType())));
+				return
+					hxcoro.Coro.suspend(cont -> {
+						cpp.asys.Net.reverse(
+							cpp.asys.Context.get(),
+							raw.getData(),
+							host -> cont.succeedAsync([ host ]),
+							msg -> cont.failAsync(new IoException(msg.toIoErrorType())));
+					});
 		}
 	}
 

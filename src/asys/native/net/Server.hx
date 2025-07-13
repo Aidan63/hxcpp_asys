00c8b05a6aa92fce2347f709a1fbc9f504145ea2
Aidan Lee <aidan.lee@evcam.com>
2025-07-13 22:12:04 +0100

    tcp server

--- src/asys/native/net/Server.hx
+++ src/asys/native/net/Server.hx
@@ -8,6 +8,8 @@ import haxe.Callback;
 import asys.native.net.Ip.IpTools;
 import sys.thread.Thread;
 
+using hxcoro.util.Convenience;
+
 typedef ServerOptions = SocketOptions & {
 	/**
 		Maximum size of incoming connections queue.
@@ -28,136 +30,121 @@ class Server {
         this.address = SocketAddress.Net(native.localAddress.host, native.localAddress.port);
     }
 
-    public function accept(callback:Callback<Socket>) {
-		if (callback == null) {
-			throw new ArgumentException("callback", "callback was null");
-		}
-
-        native.accept(
-            socket -> callback.success(@:privateAccess new asys.native.net.Socket.TcpSocketSpecialisation(socket)),
-            msg -> callback.fail(new IoException(msg)));
+    @:coroutine public function accept():Socket {
+		return hxcoro.Coro.suspend(cont -> {
+			native.accept(
+				socket -> cont.succeedAsync(@:privateAccess new asys.native.net.Socket.TcpSocketSpecialisation(socket)),
+				msg -> cont.failAsync(new IoException(msg)));
+		});
     }
 
-    public function close(callback:Callback<NoData>) {
-		if (callback == null) {
-			throw new ArgumentException("callback", "callback was null");
-		}
+    // public function close(callback:Callback<NoData>) {
+	// 	if (callback == null) {
+	// 		throw new ArgumentException("callback", "callback was null");
+	// 	}
 
-        native.close(
-            () -> callback.success(null),
-            msg -> callback.fail(new IoException(msg)));
-    }
+    //     native.close(
+    //         () -> callback.success(null),
+    //         msg -> callback.fail(new IoException(msg)));
+    // }
 
-	/**
-		Get the value of a specified socket option.
-	**/
-	public function getOption<T>(option:SocketOptionKind<T>, callback:Callback<T>) {
-		if (callback == null) {
-			throw new ArgumentException("callback", "callback was null");
-		}
+	// /**
+	// 	Get the value of a specified socket option.
+	// **/
+	// public function getOption<T>(option:SocketOptionKind<T>, callback:Callback<T>) {
+	// 	if (callback == null) {
+	// 		throw new ArgumentException("callback", "callback was null");
+	// 	}
 
-		switch option {
-			case KeepAlive:
-				native.getKeepAlive(
-					callback.success,
-					msg -> callback.fail(new IoException(msg)));
-			case SendBuffer:
-				native.getSendBufferSize(
-					callback.success,
-					msg -> callback.fail(new IoException(msg)));
-			case ReceiveBuffer:
-				native.getRecvBufferSize(
-					callback.success,
-					msg -> callback.fail(new IoException(msg)));
-			case _:
-				callback.fail(new NotImplementedException());
-		}
-	}
+	// 	switch option {
+	// 		case KeepAlive:
+	// 			native.getKeepAlive(
+	// 				callback.success,
+	// 				msg -> callback.fail(new IoException(msg)));
+	// 		case SendBuffer:
+	// 			native.getSendBufferSize(
+	// 				callback.success,
+	// 				msg -> callback.fail(new IoException(msg)));
+	// 		case ReceiveBuffer:
+	// 			native.getRecvBufferSize(
+	// 				callback.success,
+	// 				msg -> callback.fail(new IoException(msg)));
+	// 		case _:
+	// 			callback.fail(new NotImplementedException());
+	// 	}
+	// }
 
-	/**
-		Set socket option.
-	**/
-	public function setOption<T>(option:SocketOptionKind<T>, value:T, callback:Callback<NoData>) {
-		if (callback == null) {
-			throw new ArgumentException("callback", "callback was null");
-		}
+	// /**
+	// 	Set socket option.
+	// **/
+	// public function setOption<T>(option:SocketOptionKind<T>, value:T, callback:Callback<NoData>) {
+	// 	if (callback == null) {
+	// 		throw new ArgumentException("callback", "callback was null");
+	// 	}
 
-		switch option {
-			case KeepAlive:
-				native.setKeepAlive(
-					value,
-					() -> callback.success(null),
-					msg -> callback.fail(new IoException(msg)));
-			case SendBuffer:
-				native.setSendBufferSize(
-					value,
-					() -> callback.success(null),
-					msg -> callback.fail(new IoException(msg)));
-			case ReceiveBuffer:
-				native.setRecvBufferSize(
-					value,
-					() -> callback.success(null),
-					msg -> callback.fail(new IoException(msg)));
-			case _:
-				callback.fail(new NotImplementedException());
-		}
-	}
+	// 	switch option {
+	// 		case KeepAlive:
+	// 			native.setKeepAlive(
+	// 				value,
+	// 				() -> callback.success(null),
+	// 				msg -> callback.fail(new IoException(msg)));
+	// 		case SendBuffer:
+	// 			native.setSendBufferSize(
+	// 				value,
+	// 				() -> callback.success(null),
+	// 				msg -> callback.fail(new IoException(msg)));
+	// 		case ReceiveBuffer:
+	// 			native.setRecvBufferSize(
+	// 				value,
+	// 				() -> callback.success(null),
+	// 				msg -> callback.fail(new IoException(msg)));
+	// 		case _:
+	// 			callback.fail(new NotImplementedException());
+	// 	}
+	// }
 
-    static public function open(address:SocketAddress, ?options:ServerOptions, callback:Callback<Server>) {
-		if (callback == null) {
-			throw new ArgumentException("callback", "callback was null");
-		}
-
+    @:coroutine static public function open(address:SocketAddress, options:Null<ServerOptions>):Server {
 		if (address == null) {
-			callback.fail(new ArgumentException("address", "address was null"));
-
-			return;
+			throw new ArgumentException("address", "address was null");
 		}
 
         switch address {
             case Net(host, port):
 				if (host == null) {
-					callback.fail(new ArgumentException("host", "Net SocketAddress host was null"));
-
-					return;
+					throw new ArgumentException("host", "Net SocketAddress host was null");
 				}
 
 				if (options != null && options.backlog <= 0) {
-					callback.fail(new ArgumentException("backlog", "Backlog must be greater than zero"));
-
-					return;
+					throw new ArgumentException("backlog", "Backlog must be greater than zero");
 				}
 
-                try {
-                    switch IpTools.parseIp(host) {
-						case Ipv4(_):
-                            cpp.asys.TcpServer.open_ipv4(
-                                @:privateAccess Thread.current().context(),
-                                host,
-                                port,
-                                options,
-                                server -> callback.success(new Server(server)),
-                                msg -> callback.fail(new IoException(msg)));
-						case Ipv6(_):
-							cpp.asys.TcpServer.open_ipv6(
-                                @:privateAccess Thread.current().context(),
-                                host,
-                                port,
-                                options,
-                                server -> callback.success(new Server(server)),
-                                msg -> callback.fail(new IoException(msg)));
-                    }
-                } catch (exn) {
-                    callback.fail(exn);
-                }
+                switch IpTools.parseIp(host) {
+					case Ipv4(_):
+						return hxcoro.Coro.suspend(cont -> {
+							cpp.asys.TcpServer.open_ipv4(
+								cpp.asys.Context.get(),
+								host,
+								port,
+								options,
+								server -> cont.succeedAsync(new Server(server)),
+								msg -> cont.failAsync(new IoException(msg)));
+						});
+					case Ipv6(_):
+						throw new NotImplementedException();
+						// cpp.asys.TcpServer.open_ipv6(
+						//     cpp.asys.Context.get(),
+						//     host,
+						//     port,
+						//     options,
+						//     server -> callback.success(new Server(server)),
+						//     msg -> callback.fail(new IoException(msg)));
+				}
             case Ipc(path):
 				if (path == null) {
-					callback.fail(new ArgumentException("path", "Ipc SocketAddress path was null"));
-
-					return;
+					throw new ArgumentException("path", "Ipc SocketAddress path was null");
 				}
 
-				callback.fail(new NotImplementedException());
+				throw new NotImplementedException();
                 // cpp.asys.Server.open_ipc(
                 //     @:privateAccess Thread.current().context(),
                 //     path,
